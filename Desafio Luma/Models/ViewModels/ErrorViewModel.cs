5e92e72d0581866c8e65443a9f6cd9212068119f
mateus oliveira <mateus.gald@gmail.com>
2022-02-07 16:21:18 -0300

    inicialização do projeto

---
+++ Desafio Luma/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Desafio_Luma.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
